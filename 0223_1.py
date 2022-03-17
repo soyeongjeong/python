3098472d25d9266189c3676d7d4a5d611fda0627
soyeongjeong <96771235+soyeongjeong@users.noreply.github.com>
2022-03-17 13:56:06 +0900

    Create 0223_1.py

---
+++ 0223_1.py
@@ -0,0 +1,102 @@
+  
+# row = 0
+# cnt = 0
+
+
+# while row < 5:
+#     row = row + 1
+#     cnt = 1
+    
+#     if cnt < row + 1:
+#         print (" ", end = " ")
+        
+#         cnt = cnt + 1
+    
+#     elif cnt == row:
+#         print ("*")
+    
+#     print (" ")
+    
+       
+    
+    
+    
+    
+
+# #-------------------------
+    
+    
+  
+# row = 0
+# cnt = 0
+
+
+# while row < 5:
+#     row = row + 1
+#     cnt = 5
+    
+   
+    
+#     while cnt > row:
+        
+        
+        
+#         print(" ", end= " ")
+    
+       
+        
+#         if cnt <= row:
+#             print ("*")
+            
+#         cnt = cnt - 1
+        
+
+        
+#     print ("*")
+    
+    
+    
+    
+    
+
+#-------------------------
+
+    
+    
+  
+
+
+
+row = 0
+cnt = 0
+
+
+while row < 5:
+    row = row + 1
+    cnt = 5
+    
+    while cnt>  0:
+        if cnt<=row :
+            print ("*", end = "")
+            
+        else:
+            print (" ", end = "")
+    
+        cnt = cnt - 1   
+         
+    print()      
+
+    
+        
+#======== 삼각형 별 출력 ========   
+    
+#     *
+#    **
+#   ***
+#  ****
+# *****
+  
+    
+
+
+       
